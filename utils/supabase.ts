3890d51b0e3b9d01cefd2d60efedc587fdd38cf4
= <=>
2024-08-26 12:19:35 +0500

    commit

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -3,23 +3,23 @@ import { createClient } from "@supabase/supabase-js";
 const bucket = "main-bucket";
 
 export const supabase = createClient(
-    process.env.SUPABASE_URL as string,
-    process.env.SUPABASE_KEY as string
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY as string
 );
 
 export const uploadImage = async (image: File) => {
-    const timestamp = Date.now();
-    const newName = `${timestamp}-${image.name}`;
+  const timestamp = Date.now();
+  const newName = `${timestamp}-${image.name}`;
 
-    const { data } = await supabase.storage
-        .from(bucket)
-        .upload(newName, image, { cacheControl: "3600" });
-    if (!data) throw new Error("Image upload failed");
-    return supabase.storage.from(bucket).getPublicUrl(newName).data.publicUrl;
+  const { data } = await supabase.storage
+    .from(bucket)
+    .upload(newName, image, { cacheControl: "3600" });
+  if (!data) throw new Error("Image upload failed");
+  return supabase.storage.from(bucket).getPublicUrl(newName).data.publicUrl;
 };
 
 export const deleteImage = async (url: string) => {
-    const imageName = url.split("/").pop();
-    if (!imageName) throw new Error("Invalid URL");
-    return supabase.storage.from(bucket).remove([imageName]);
+  const imageName = url.split("/").pop();
+  if (!imageName) throw new Error("Invalid URL");
+  return supabase.storage.from(bucket).remove([imageName]);
 };
